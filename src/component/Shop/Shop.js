dc204d9006e04f33cbf41631fa47b412365e5b49
mahmudulhasan22 <mahmdulhasan001@gmail.com>
2022-12-27 16:17:21 +0600

    aded product component and shop

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    const []
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <h1>product container</h1>
+            </div>
+            <div className='cart-container'>
+                <h1>cart container</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
